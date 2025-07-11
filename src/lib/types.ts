f6c412ab5538723ece2789dd3e7a0200af94c6c3
marcxsdev <marcosalexandre1322@gmail.com>
2025-07-10 22:41:56 -0300

    chore: add all cards

---
+++ src/lib/types.ts
@@ -0,0 +1,5 @@
+export interface Card {
+  id: number; // Um identificador único para cada par de cartas (ex: 'frio-quente')
+  left: string; // Ex: "Melhor frio"
+  right: string; // Ex: "Melhor quente"
+}
