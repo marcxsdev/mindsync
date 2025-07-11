be4a61738adc6f5f9688a6e583fdbb211232b1a6
marcxsdev <marcosalexandre1322@gmail.com>
2025-07-10 21:30:17 -0300

    fix: add cards component

--- src/components/game/Cards.tsx
+++ src/components/game/Cards.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Cards = () => {
   return (
     <div className="flex flex-row gap-5">
-      <div className="bg-linear-to-r from-[#0B6AC7] to-[#00BFFF] w-[513px] h-[157px]"></div>
-      <div className="bg-linear-to-r from-[#FF9B00] to-[#FF4500] w-[513px] h-[157px]"></div>
+      <div className="bg-linear-to-r from-[#0B6AC7] to-[#00BFFF] w-[513px] h-[157px] rounded-[40px]"></div>
+      <div className="bg-linear-to-r from-[#FF9B00] to-[#FF4500] w-[513px] h-[157px] rounded-[40px]"></div>
     </div>
   );
 };
