c59161f99f72847ff801d59e47dc64494db60390
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-01 08:43:16 -0300

    chores: folder estructure

---
+++ src/app/game/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GamePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold mb-8">MindSync Game</h1>
+      {/* Aqui entrará a lógica e os componentes do jogo */}
+      <p>Tela do jogo em construção...</p>
+    </div>
+  );
+}
