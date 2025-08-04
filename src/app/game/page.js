5fb46c278218d9cf8110c32c0244de02bff4a532
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-04 20:23:10 -0300

    feat: add button component

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,14 +1,17 @@
+"use client";
+
 import Slider from "@/components/game/Slider";
+import Button from "@/components/ui/Button";
 import React from "react";
 
 export default function GamePage() {
   return (
     <div className="flex flex-col text-white items-center justify-center min-h-screen p-4">
       <h1 className="text-4xl text-white font-bold mb-8">MindSync Game</h1>
-      {/* Aqui entrará a lógica e os componentes do jogo */}
       <p>Tela do jogo em construção...</p>
 
       <Slider />
+      <Button onClick={() => console.log("teste")}>REVELAR!</Button>
     </div>
   );
 }
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-[#e94560] text-white border-0 rounded-xl px-18 py-6 text-2xl font-bold uppercase cursor-pointer transition-all duration-300 ease-in-out shadow-lg hover:transform hover:scale-105"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
