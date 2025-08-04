22a06be044304216702bc45dfb75a3e5e78a634e
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-04 20:40:08 -0300

    feat: add game board component

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,17 +1,9 @@
-"use client";
-
-import Slider from "@/components/game/Slider";
-import Button from "@/components/ui/Button";
-import React from "react";
+import GameBoard from "@/components/game/GameBoard";
 
 export default function GamePage() {
   return (
     <div className="flex flex-col text-white items-center justify-center min-h-screen p-4">
-      <h1 className="text-4xl text-white font-bold mb-8">MindSync Game</h1>
-      <p>Tela do jogo em construção...</p>
-
-      <Slider />
-      <Button onClick={() => console.log("teste")}>REVELAR!</Button>
+      <GameBoard />
     </div>
   );
 }
--- src/components/game/Slider.jsx
+++ src/components/game/Slider.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState, useRef } from "react";
 
 const Slider = () => {
@@ -10,35 +9,31 @@ const Slider = () => {
   };
 
   return (
-    <div className="w-full max-w-4xl rounded-2xl bg-slate-800 p-8 shadow-2xl border border-blue-900">
-      <main>
+    <main>
+      <div ref={sliderRef} className="relative h-40 rounded-xl overflow-hidden">
         <div
-          ref={sliderRef}
-          className="relative h-40 rounded-xl overflow-hidden"
-        >
-          <div
-            className="absolute inset-0 
+          className="absolute inset-0 
             bg-gradient-to-r from-red-500 via-purple-700 to-blue-700 
             transition-opacity duration-500 ease-in-out"
-          ></div>
+        ></div>
 
-          <div
-            className="absolute top-[-10px] bottom-[-10px] w-1 
+        <div
+          className="absolute top-[-10px] bottom-[-10px] w-1 
                        bg-white rounded-sm shadow-lg shadow-white/70 
                        pointer-events-none z-20"
-            style={{
-              left: `${markerPosition}%`,
-              transform: "translateX(-50%)",
-            }}
-          ></div>
+          style={{
+            left: `${markerPosition}%`,
+            transform: "translateX(-50%)",
+          }}
+        ></div>
 
-          <input
-            type="range"
-            min="0"
-            max="100"
-            value={markerPosition}
-            onChange={handleSliderChange}
-            className="absolute inset-0 w-full h-full 
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={markerPosition}
+          onChange={handleSliderChange}
+          className="absolute inset-0 w-full h-full 
                        appearance-none bg-transparent cursor-pointer z-30
                        [&::-webkit-slider-thumb]:appearance-none 
                        [&::-webkit-slider-thumb]:w-2 
@@ -48,11 +43,10 @@ const Slider = () => {
                        [&::-moz-range-thumb]:h-full 
                        [&::-moz-range-thumb]:bg-transparent 
                        [&::-moz-range-thumb]:border-none"
-            id="guessSlider"
-          />
-        </div>
-      </main>
-    </div>
+          id="guessSlider"
+        />
+      </div>
+    </main>
   );
 };
 
